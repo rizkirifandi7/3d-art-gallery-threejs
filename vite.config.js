33f93c83df5790e5d86597e35be8fb8795ad0e2c
rizkirifandi7 <rizkirifandi7@gmail.com>
2025-02-12 07:36:51 +0700

    Update package.json scripts, modify Vite base path, and add vercel.json for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
+// filepath: /c:/IKI/project-learning/3D-art-gallery-threejs-master/vite.config.js
 import { defineConfig } from "vite";
 
 export default defineConfig({
-	base: "/3d-art-gallery-threejs",
+	base: "/",
+	assetsInclude: ["**/*.jpg", "**/*.png", "**/*.glb", "**/*.gltf"],
 	build: {
 		rollupOptions: {
 			input: "/main.js",
